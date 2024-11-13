1d5e7fa0fa550efb5dd72128b3c00e2ee7434edd
Student <Student@412-09>
2024-11-13 13:57:23 +0300

    Добавьте файлы проекта.

---
+++ start28/RegistrationWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace start28
+{
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = FirstNameTextBox.Text;
+            string lastName = LastNameTextBox.Text;
+            DateTime? birthDate = BirthDatePicker.SelectedDate;
+            string teamName = TeamNameTextBox.Text;
+            string category = (CategoryComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            if (string.IsNullOrEmpty(firstName) || string.IsNullOrEmpty(lastName) || !birthDate.HasValue ||
+                string.IsNullOrEmpty(teamName) || string.IsNullOrEmpty(category))
+            {
+                MessageBox.Show("Пожалуйста, заполните все поля.");
+                return;
+            }
+
+            
+
+            MessageBox.Show("Регистрация успешна!");
+            this.Close(); // Закрыть окно после успешной регистрации
+        }
+    }
+}
